9d72bc2a09cfecb217b749eb3c48ed2f1ea3048e
BuildTools <jamie.gardner25@gmail.com>
2023-12-11 21:04:16 -0800

    climb

--- src/main/java/net/gardna/james/autotreechopper/TreeChopper.java
+++ src/main/java/net/gardna/james/autotreechopper/TreeChopper.java
@@ -46,6 +46,7 @@ public class TreeChopper {
         vector = direction.getFacing().getDirection();
         // get location of bit block
         Block bit = chest.getRelative(BlockFace.DOWN);
+        // set location of tree chopper to bit block
         location = bit.getLocation();
         location.setDirection(direction.getFacing().getDirection());
 
@@ -53,7 +54,7 @@ public class TreeChopper {
         armorStandLocation.setDirection(direction.getFacing().getDirection());
 
         // replace chest with armor stand
-        armorStand = chest.getWorld().spawn(armorStandLocation, ArmorStand.class);
+        armorStand = chest.getWorld().spawn(bit.getLocation().add(.5, -.35, .5).setDirection(location.getDirection()), ArmorStand.class);
         chest.setType(Material.AIR);
         armorStand.setVisible(false);
         armorStand.setGravity(false);
@@ -64,15 +65,17 @@ public class TreeChopper {
         assert equipment != null;
         equipment.setHelmet(new ItemStack(Material.CHEST));
 
+        // replace bit block with falling block and add it to an armor stand
         fallingBlock = chest.getWorld().spawnFallingBlock(bit.getLocation(), bit.getBlockData());
         fallingBlock.setGravity(false);
-        fallingBlockArmorStand = chest.getWorld().spawn(bit.getLocation().add(.5, -2, .5), ArmorStand.class);
+        fallingBlockArmorStand = chest.getWorld().spawn(bit.getLocation().add(.5, -2, .5).setDirection(location.getDirection()), ArmorStand.class);
         fallingBlockArmorStand.setVisible(false);
         fallingBlockArmorStand.setGravity(false);
         fallingBlockArmorStand.setInvulnerable(true);
         fallingBlockArmorStand.setCanPickupItems(false);
         fallingBlockArmorStand.addPassenger(fallingBlock);
 
+        // schedule update() to run every tick
         BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
         scheduler.scheduleSyncRepeatingTask(Main.getPlugin(Main.class), this::update, 40L, 1L);
     }
@@ -106,17 +109,44 @@ public class TreeChopper {
         // move armor stand and falling block to the chest
         location = location.add(new Vector(-.1, -.1, -.1).multiply(vector));
 
-        Vector vector = location.toVector();
-        armorStand.teleport(vector.toLocation(armorStand.getWorld()).add(.5, -0.35, .5).setDirection(location.getDirection()));
-        Bukkit.getLogger().info("location: " + location);
+        Vector tempVector = location.toVector();
+        armorStand.teleport(tempVector.toLocation(armorStand.getWorld()).add(.5, -0.35, .5).setDirection(location.getDirection()));
+        Bukkit.getLogger().info("location: " + location + " firstHalf: " + firstHalf);
 
         // move falling block armor stand, must eject and re-add passenger to update position
         fallingBlockArmorStand.eject();
-        fallingBlockArmorStand.teleport(vector.toLocation(armorStand.getWorld()).add(.5, -2, .5).setDirection(location.getDirection()));
+        fallingBlockArmorStand.teleport(tempVector.toLocation(armorStand.getWorld()).add(.5, -2, .5).setDirection(location.getDirection()));
         fallingBlockArmorStand.addPassenger(fallingBlock);
 
         // set falling block to 1 tick lived so it doesn't break
         fallingBlock.setTicksLived(1);
+
+        if (location.getX() % 1 > .5 || location.getZ() % 1 > .5) {
+            if (firstHalf) {
+                Bukkit.getLogger().info("climbOrDescend");
+                climbOrDescend();
+                firstHalf = false;
+            }
+        } else {
+            firstHalf = true;
+        }
+    }
+
+    private void climbOrDescend() {
+        Vector tempVector = location.toVector().add(new Vector(-1, -1, -1).multiply(vector));
+        Block block = tempVector.toLocation(armorStand.getWorld()).getBlock();
+        // if block is air, return
+        if (block.getType() == Material.AIR) {
+            return;
+        }
+        // if block has a block above it, destroy tree chopper
+        if (block.getRelative(BlockFace.UP).getType() != Material.AIR) {
+            // cant climb two blocks, so destroy tree chopper
+            destroy();
+        } else {
+            // climb up one block
+            location = location.add(0, 1, 0);
+        }
     }
 
     /**
